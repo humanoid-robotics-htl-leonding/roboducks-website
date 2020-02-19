13401a8fee9cba29301d5db7a58abc901075dd22
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-02-19 13:27:47 +0100

    First version of deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [[ -d ../public ]]
+then
+  echo "Cleaning destination directory ... "
+  rm -r ../public
+  echo "done"
+fi
+echo "Generating web site ... "
+hugo -d ../public
+echo "done"
+
+echo "Copying files to remote server ... "
+scp -r ../public/* bauer@roboducks.htl-leonding.ac.at:/home/bauer
+echo "done"
